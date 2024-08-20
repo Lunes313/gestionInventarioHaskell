0bf2fd87e363843268f7108cb5f2c4ad8f9da20d
Lunes313 <99741938+Lunes313@users.noreply.github.com>
2024-08-19 20:49:47 -0500

    Codigo para realizar el inventario

--- Practice.hs
+++ Practice.hs
@@ -1,127 +1,103 @@
-import Data.Time.Clock
+import System.Directory
 import Data.List
 import System.IO
-import Control.Exception
 import Control.DeepSeq (deepseq)
 
--- Definición del tipo de datos para representar la información de un vehículo
-data Vehiculo = Vehiculo {
-    placa :: String,
-    entrada :: UTCTime,
-    salida :: Maybe UTCTime  -- Usamos Maybe para representar que el vehículo aún está en el parqueadero o ya salió
+data Producto = Producto {
+    nombre :: String,
+    categoria :: String,
+    precio :: Float
 } deriving (Show, Read)
 
--- Función para registrar la entrada de un vehículo al parqueadero
-registrarEntrada :: String -> UTCTime -> [Vehiculo] -> [Vehiculo]
-registrarEntrada placaVehiculo tiempo parqueadero =
-    Vehiculo placaVehiculo tiempo Nothing : parqueadero
-
--- Función para registrar la salida de un vehículo del parqueadero
-registrarSalida :: String -> UTCTime -> [Vehiculo] -> [Vehiculo]
-registrarSalida placaVehiculo tiempo parqueadero =
-    map (\v -> if placaVehiculo == placa v then v { salida = Just tiempo } else v) parqueadero
-
--- Función para buscar un vehículo por su placa en el parqueadero
-buscarVehiculo :: String -> [Vehiculo] -> Maybe Vehiculo
-buscarVehiculo placaVehiculo parqueadero =
-    find (\v -> placaVehiculo == placa v && isNothing (salida v)) parqueadero
-    where
-        isNothing Nothing = True
-        isNothing _       = False
-
--- Función para calcular el tiempo que un vehículo permaneció en el parqueadero
-tiempoEnParqueadero :: Vehiculo -> IO NominalDiffTime
-tiempoEnParqueadero vehiculo = do
-    tiempoActual <- getCurrentTime
-    return $ diffUTCTime tiempoActual (entrada vehiculo)
-
--- Función para guardar la información de los vehículos en un archivo de texto
-guardarParqueadero :: [Vehiculo] -> IO ()
-guardarParqueadero parqueadero = do
-    withFile "parqueadero.txt" WriteMode $ \h -> do
-        hPutStr h (unlines (map mostrarVehiculo parqueadero))
-    putStrLn "Parqueadero guardado en el archivo parqueadero.txt."
-
--- Función para cargar la información de los vehículos desde un archivo de texto
-cargarParqueadero :: IO [Vehiculo]
-cargarParqueadero = do
-    contenido <- withFile "parqueadero.txt" ReadMode $ \h -> do
+-- Función para cargar la información del inventario desde un archivo de texto
+leerInventario :: IO [Producto]
+leerInventario = do
+    contenido <- withFile "inventario.txt" ReadMode $ \h -> do
         contenido <- hGetContents h
+        -- Evalua el contenido para forzar la lectura del archivo
         contenido `deepseq` return contenido
     let lineas = lines contenido
-    return (map leerVehiculo lineas)
+    -- Convierte las líneas del archivo en una lista de productos
+    return (map leerProducto lineas)
     where
-        leerVehiculo linea = read linea :: Vehiculo
+        leerProducto linea = read linea :: Producto
 
--- Función para mostrar la información de un vehículo como cadena de texto
-mostrarVehiculo :: Vehiculo -> String
-mostrarVehiculo (Vehiculo placa entrada salida) =
-    "Vehiculo {placa = \"" ++ placa ++ "\", entrada = " ++ show entrada ++ ", salida = " ++ maybe "Nothing" show salida ++ "}"
+-- Función para guardar los productos en un archivo de texto
+guardarInventario :: [Producto] -> IO ()
+guardarInventario inventario = do
+    let filePath = "inventario.txt"
+    -- Escribe el inventario en el archivo
+    withFile filePath WriteMode $ \h -> do
+        -- Convierte los productos en cadenas de texto y las escribe en el archivo
+        hPutStr h (unlines (map show inventario))
+    putStrLn "Inventario guardado en el archivo inventario.txt."
 
--- Función para listar los vehículos en el parqueadero
-listarVehiculos :: [Vehiculo] -> IO ()
-listarVehiculos [] = putStrLn "No hay vehículos en el parqueadero."
-listarVehiculos vehiculos = do
-    putStrLn "Vehículos en el parqueadero:"
-    mapM_ (putStrLn . mostrarVehiculo) vehiculos
+-- Funcion para mostrar los productos
+mostrarProducto :: Producto -> String
+mostrarProducto (Producto nombre categoria precio) =
+    "Producto: " ++ nombre ++ "\ncategoria: " ++ categoria ++ "\nprecio: " ++ show precio ++ "\n"
+    
+-- Funcion para buscar productos por categoria
+buscarPorCategoria :: [Producto] ->     String -> [Producto]
+buscarPorCategoria inventario cat = filter (\producto -> cat == categoria producto) inventario
 
--- Función principal del programa
+-- Funcion para listar todos los productos
+listarProductos :: [Producto] -> IO ()
+listarProductos [] = putStrLn "No hay productos en el inventario"
+listarProductos inventario = do
+    putStrLn "\nProductos en el inventario:\n"
+    -- Muestra cada producto en el inventario
+    mapM_ (putStrLn . mostrarProducto) inventario
+
+
+-- Funcion para mostrar la cantidad de productos por categoria
+cantidadPorCategoria :: [Producto] -> String -> Int
+cantidadPorCategoria inventario cat = length (buscarPorCategoria inventario cat)
+
+
 main :: IO ()
 main = do
-    -- Cargar el parqueadero desde el archivo de texto
-    parqueadero <- cargarParqueadero
-    putStrLn "¡Bienvenido al Sistema de Gestión de Parqueadero!"
+    inventario <- leerInventario
+    putStrLn "!Bienvenido al Sistema de Gestión de Inventario!"
+    menu inventario
 
-    -- Ciclo principal del programa
-    cicloPrincipal parqueadero
-
--- Función para el ciclo principal del programa
-cicloPrincipal :: [Vehiculo] -> IO ()
-cicloPrincipal parqueadero = do
+menu :: [Producto] -> IO ()
+menu inventario = do
     putStrLn "\nSeleccione una opción:"
-    putStrLn "1. Registrar entrada de vehículo"
-    putStrLn "2. Registrar salida de vehículo"
-    putStrLn "3. Buscar vehículo por placa"
-    putStrLn "4. Listar vehículos"
+    putStrLn "1. Añadir producto"
+    putStrLn "2. Buscar artículos por categoría"
+    putStrLn "3. Listar todos los artículos"
+    putStrLn "4. Mostrar cantidad de artículos por categoría" 
     putStrLn "5. Salir"
-
     opcion <- getLine
     case opcion of
         "1" -> do
-            putStrLn "Ingrese la placa del vehículo:"
-            placaVehiculo <- getLine
-            tiempoActual <- getCurrentTime
-            let parqueaderoActualizado = registrarEntrada placaVehiculo tiempoActual parqueadero
-            putStrLn $ "Vehículo con placa " ++ placaVehiculo ++ " ingresado al parqueadero."
-            guardarParqueadero parqueaderoActualizado
-            cicloPrincipal parqueaderoActualizado
-
+            putStrLn "Nombre: "
+            nombre <- getLine
+            putStrLn "Categoria: "
+            categoria <- getLine
+            putStrLn "Precio: "
+            precio <- getLine
+            let producto = Producto nombre categoria (read precio :: Float)
+            guardarInventario (inventario ++ [producto])
+            menu (inventario ++ [producto])
         "2" -> do
-            putStrLn "Ingrese la placa del vehículo a salir:"
-            placaVehiculo <- getLine
-            tiempoActual <- getCurrentTime
-            let parqueaderoActualizado = registrarSalida placaVehiculo tiempoActual parqueadero
-            putStrLn $ "Vehículo con placa " ++ placaVehiculo ++ " salido del parqueadero."
-            guardarParqueadero parqueaderoActualizado
-            cicloPrincipal parqueaderoActualizado
-
+            putStrLn "Categoria: "
+            categoria <- getLine
+            let productos = buscarPorCategoria inventario categoria
+            listarProductos productos
+            menu inventario
         "3" -> do
-            putStrLn "Ingrese la placa del vehículo a buscar:"
-            placaVehiculo <- getLine
-            case buscarVehiculo placaVehiculo parqueadero of
-                Just vehiculo -> do
-                    tiempoTotal <- tiempoEnParqueadero vehiculo
-                    putStrLn $ "El vehículo con placa " ++ placaVehiculo ++ " se encuentra en el parqueadero."
-                    putStrLn $ "Tiempo en parqueadero: " ++ show tiempoTotal ++ " segundos."
-                Nothing -> putStrLn "Vehículo no encontrado en el parqueadero."
-            cicloPrincipal parqueadero
-
+            listarProductos inventario
+            menu inventario
         "4" -> do
-            listarVehiculos parqueadero
-            cicloPrincipal parqueadero
-
-        "5" -> putStrLn "¡Hasta luego!"
-
+            putStrLn "Categoria: "
+            categoria <- getLine
+            let cantidad = cantidadPorCategoria inventario categoria
+            putStrLn $ "Cantidad de productos en la categoria " ++ categoria ++ ": " ++ show cantidad
+            menu inventario
+        "5" -> do
+            putStrLn "Adios"
         _ -> do
-            putStrLn "Opción no válida. Por favor, seleccione una opción válida."
-            cicloPrincipal parqueadero
+            putStrLn "Opcion invalida"
+            menu inventario
